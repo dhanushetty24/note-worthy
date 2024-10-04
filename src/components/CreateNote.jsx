c56977c5e791e89cc5dcb309743a051525a52be1
dhanushetty24 <dhanushetty248@gmail.com>
2024-10-04 20:03:08 +0530

    Update List added

--- src/components/CreateNote.jsx
+++ src/components/CreateNote.jsx
@@ -3,21 +3,21 @@ import Notes from "./Notes";
 import styles from '../styles/CreateNote.module.css'
 
 const CreateNote = () => {
-  const [cardData, setCardData] = useState({ title: '', note: '' });
+  const [cardData, setCardData] = useState({ title: '', note: '', id: ''});
   const [notes, setNotes] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setNotes([...notes, cardData]);
-    setCardData({ title: '', note: '' });
+    setCardData({ title: '', note: '', id: ''});
   };
 
   const handleInputChange = (e) => {
-    setCardData({ ...cardData, [e.target.name]: e.target.value });
+    setCardData({ ...cardData, id: Math.floor(Math.random() * Date.now()),  [e.target.name]: e.target.value });
   }
   
-  const handleOnClick = () => {
-    
+  const handleOnClick = (id) => {
+    setNotes(notes.filter((note) => note.id !== id));
   } 
 
   return (
@@ -45,7 +45,7 @@ const CreateNote = () => {
           </div>
         </form>
         <ul className={styles.displayStyle}>
-          {notes.length ? notes.map((obj) => <Notes cardData = {obj}/>) : false}
+          {notes.length ? notes.map((obj) => <Notes key ={obj.id} handleOnClick = {handleOnClick} cardData = {obj}/>) : false}
         </ul>
     </section>
   )
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,10 +1,11 @@
 import styles from '../styles/Notes.module.css'
 
-const Notes = ({cardData}) => {
+const Notes = ({cardData, handleOnClick}) => {
   return (
     <li className={styles.card}>
-      <h3>{cardData.title}</h3>
-      <p>{cardData.note}</p>
+      <h3>{cardData.title.length>20 ? `${cardData.title.slice(0,18)}...` : cardData.title }</h3>
+      <p>{cardData.note.length>35 ? `${cardData.note.slice(0,31)}...` : cardData.note }</p>
+      <button className= {styles.closeButton} onClick={() =>handleOnClick(cardData.id) }>X</button>
     </li>
   );
 };
