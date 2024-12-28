62e7da965f3ae2c3221824948321c9700ddd76ae
dhanushetty24 <dhanushetty248@gmail.com>
2024-12-28 13:06:14 +0530

    useReducer hook used for formData and few logic changes added for read notes

--- src/components/CreateNote.jsx
+++ src/components/CreateNote.jsx
@@ -1,45 +1,58 @@
-import { useState } from 'react';
+import { useState, useReducer } from 'react';
 import Notes from './Notes';
 import Modal from './Modal';
+import { FaPencilAlt } from 'react-icons/fa';
 import styles from '../styles/CreateNote.module.css';
 
+
 const CreateNote = () => {
-  const [cardData, setCardData] = useState({ title: '', note: '', id: '' });
   const [notes, setNotes] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isNoteOpen, setIsNoteOpen] = useState(false);
+  const [displayWarn, setDisplayWarn] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setNotes([
-      ...notes,
-      { ...cardData, id: Math.floor(Math.random() * Date.now()) },
-    ]);
-    setCardData({ title: '', note: '', id: '' });
-    setIsModalOpen(false);
-  };
+const initialState = { title: '', note: '', id: '' }
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'UPDATE_TITLE':
+      {
+        action.payload.length === 0 ? setDisplayWarn(true) : setDisplayWarn(false);
+        return { ...state, title: action.payload };
+      }
+    case 'UPDATE_NOTE':
+      return { ...state, note: action.payload };
+    case 'SET_DISPLAY':
+      return action.payload;
+    case 'RESET':
+      return initialState;
+    default:
+      return state;
+  }
+}
+const [cardDataSet, dispathCardData] = useReducer(reducer, initialState);
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setCardData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
+const handleSubmit = (e) => {
+  e.preventDefault();
+  setNotes([
+    ...notes,
+    { ...cardDataSet, id: Math.floor(Math.random() * Date.now()) },
+  ]);
+  dispathCardData({ type: 'RESET' });
+  setIsModalOpen(false);
+};
 
   const handleDeleteNote = (id) => {
-    setCardData({ title: '', note: '', id: '' });
+    dispathCardData({ type: 'RESET' });
     setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
   };
 
   const handleNoteOpen = (id) => {
     const selectedNote = notes.find((note) => note.id === id);
-    setCardData(selectedNote);
+    dispathCardData({ type: 'SET_DISPLAY', payload: selectedNote});
     setIsNoteOpen(true);
   };
 
   const handleNoteClose = () => {
-    setCardData({ title: '', note: '', id: '' });
     setIsNoteOpen(false);
   };
 
@@ -51,8 +64,22 @@ const CreateNote = () => {
     setIsModalOpen(false);
   };
 
+  const handleNoteEdit = () => {
+    setIsNoteOpen(false);
+    setIsModalOpen(true);
+  };
+
+  const displayCreate = () => {
+    return (
+      cardDataSet.title.length === 0 ||
+      cardDataSet.note.length > 500 ||
+      cardDataSet.title.length > 50
+    );
+  };
+
   return (
     <section>
+      {/* Create button to create new note */}
       <div className={styles.buttonWrapper}>
         <button
           className={`${styles.addButton} ${
@@ -65,41 +92,56 @@ const CreateNote = () => {
           +
         </button>
       </div>
+      {/* Create new note modal  */}
       {isModalOpen && (
         <Modal isModalOpen={isModalOpen} onClose={handleCloseModal}>
           <form onSubmit={handleSubmit}>
-            <div>
+            <div className={styles.inputWrapper}>
               <input
                 type='text'
                 name='title'
-                value={cardData.title}
-                onChange={handleInputChange}
+                value={cardDataSet.title}
+                onChange={e =>dispathCardData({type: 'UPDATE_TITLE', payload: e.target.value})}
                 placeholder='Title'
-                required
               />
+              {cardDataSet.title.length > 50 && (
+                <p className={styles.warning}>
+                  Warning: Maximum text length of 50 characters exceeded.
+                </p>
+              )}
+              {displayWarn && (
+                <p className={styles.warning}>Warning: Title can't be empty.</p>
+              )}
             </div>
-            <div>
+            <div className={styles.inputWrapper}>
               <textarea
                 name='note'
-                value={cardData.note}
-                onChange={handleInputChange}
+                value={cardDataSet.note}
+                onChange={e => dispathCardData({type: 'UPDATE_NOTE', payload: e.target.value})}
                 placeholder='Add your Note'
-                required
               />
+              {cardDataSet.note.length > 500 && (
+                <p className={styles.warning}>
+                  Warning: Maximum text length of 500 characters exceeded.
+                </p>
+              )}
             </div>
-            <div className={styles.buttonWrapper}>
-              <button className={styles.createButton} type='submit'>
-                Create
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-              </button>
-            </div>
+            {!displayCreate() && (
+              <div className={styles.buttonWrapper}>
+                <button className={styles.createButton} type='submit'>
+                  Create
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </button>
+              </div>
+            )}
           </form>
         </Modal>
       )}
 
+      {/* Display all the notes created */}
       {notes.length > 0 && (
         <ul className={styles.displayStyle}>
           {notes.map((note) => (
@@ -113,11 +155,17 @@ const CreateNote = () => {
         </ul>
       )}
 
+      {/* Display a specific note opened in the modal*/}
       {isNoteOpen && (
         <Modal isModalOpen={isNoteOpen} onClose={handleNoteClose}>
           <div className={styles.noteOpen}>
-            <h3>{cardData.title}</h3>
-            <p>{cardData.note}</p>
+            <h3>{cardDataSet.title}</h3>
+            <p>{cardDataSet.note}</p>
+            <div className={styles.buttonWrapper}>
+              <button className={styles.editButton} onClick={handleNoteEdit}>
+                <FaPencilAlt size={25} color='white' />
+              </button>
+            </div>
           </div>
         </Modal>
       )}
